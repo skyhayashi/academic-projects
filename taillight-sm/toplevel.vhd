89391d49c54e1aa91cb4424c74e1695a3c3e3589
Skylar Hayashi <skylarhayashi@gmail.com>
2025-07-07 11:21:35 -1000

    Update toplevel.vhd

--- taillight-sm/toplevel.vhd
+++ taillight-sm/toplevel.vhd
@@ -20,16 +20,16 @@ end toplevel;
 architecture struct of toplevel is
   component taillight
   port(clk    : in std_logic;
-     rst    : in std_logic;
-     leftt  : in std_logic;
-     rightt : in std_logic;
-     hazard : in std_logic;
-     L3     : out std_logic;
-     L2     : out std_logic;
-     L1     : out std_logic;
-     R1     : out std_logic;
-     R2     : out std_logic;
-     R3     : out std_logic
+       rst    : in std_logic;
+       leftt  : in std_logic;
+       rightt : in std_logic;
+       hazard : in std_logic;
+       L3     : out std_logic;
+       L2     : out std_logic;
+       L1     : out std_logic;
+       R1     : out std_logic;
+       R2     : out std_logic;
+       R3     : out std_logic
   );
   end component;
 
