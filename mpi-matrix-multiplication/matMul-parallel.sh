62c3d7cbe7733967b46ac6b2afa6846d40cfdbeb
Skylar Hayashi <skylarhayashi@gmail.com>
2025-07-07 17:53:02 -1000

    Create matMul-parallel.sh

---
+++ mpi-matrix-multiplication/matMul-parallel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH --job-name=matMul-parallel
+#SBATCH --output=matMul-parallel.txt
+#SBATCH --nodes=4
+#SBATCH --tasks-per-node=4
+#SBATCH --time=05:00
+
+mpiexec matMul-parallel 2048
